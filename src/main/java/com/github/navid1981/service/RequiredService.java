c3e010d7f5da94ce56ea212841a790bf56b8fe6d
nsalehvaziri <nsalehvaziri@vmware.com>
2023-06-12 23:02:07 -0500

    V.0.6

--- src/main/java/com/github/navid1981/service/RequiredService.java
+++ src/main/java/com/github/navid1981/service/RequiredService.java
@@ -14,33 +14,18 @@ public class RequiredService {
 
     public String getSchema(String path, String value) throws JsonProcessingException {
         ObjectNode objectNode = (ObjectNode) new ObjectMapper().readTree(GeneratorService.schema);
-
-//        ObjectNode require =mapper.createObjectNode();
         ArrayNode arrayNode= mapper.createArrayNode();
         arrayNode.add(value);
-//        require.set("required",arrayNode);
         setJsonPointerValue(objectNode, JsonPointer.compile(path), arrayNode);
         GeneratorService.schema=objectNode.toPrettyString();
         return objectNode.toPrettyString();
     }
 
-//    public void setJsonPointerValue2(ObjectNode node, JsonPointer pointer, JsonNode value){
-//        node.set("required",value);
-//    }
     public void setJsonPointerValue(ObjectNode node, JsonPointer pointer, JsonNode value) {
-//        JsonPointer parentPointer = pointer.head();
         JsonNode parentNode = node.at(pointer);
         String fieldName = "required";
 
-        if (parentNode.isArray()) {
-            ArrayNode arrayNode = (ArrayNode) parentNode;
-            int index = Integer.valueOf(fieldName);
-            // expand array in case index is greater than array size (like JavaScript does)
-            for (int i = arrayNode.size(); i <= index; i++) {
-                arrayNode.addNull();
-            }
-            arrayNode.set(index, value);
-        } else if (parentNode.isObject()) {
+        if (parentNode.isObject()) {
             ((ObjectNode) parentNode).set(fieldName, value);
         } else {
             throw new IllegalArgumentException("`" + fieldName + "` can't be set for parent node `"
